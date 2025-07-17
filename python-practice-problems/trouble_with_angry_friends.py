8fe566092cabfb426e24319115f138af0ee898c5
Sai Venkat <81605199+saivenkatavula-wq@users.noreply.github.com>
2025-07-17 22:36:31 +0530

    Create trouble_with_angry_friends.py

---
+++ python-practice-problems/trouble_with_angry_friends.py
@@ -0,0 +1,7 @@
+def friends_in_trouble(j_angry, s_angry):
+    if j_angry == 1 and s_angry == 1:
+        return True
+    elif j_angry == 0 and s_angry == 0:
+        return True
+    else:
+        return False
