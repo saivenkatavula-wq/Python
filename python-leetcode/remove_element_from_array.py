7a772992bc74df15a5ca61c5d30169ea221d7c19
Sai Venkat <81605199+saivenkatavula-wq@users.noreply.github.com>
2025-12-30 23:47:39 +0530

    Add removeElement function to remove specified value

---
+++ python-leetcode/remove_element_from_array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for i in nums:
+            if i != val:
+                nums[k] = i
+                k += 1
+        return k
+
+        
