db47b97e32aa785e65ae9cc03cc821d01c794a15
Sai Venkat <81605199+saivenkatavula-wq@users.noreply.github.com>
2026-01-16 18:11:56 +0530

    Add alternative approach for sum of n natural numbers
    Added an alternative approach to calculate the sum of n natural numbers using the formula.

--- python-practice-problems/sum_of_n_natural_numbers.py
+++ python-practice-problems/sum_of_n_natural_numbers.py
@@ -4,4 +4,10 @@ def n_natural_numbers(n):
     return n + n_natural_numbers(n-1)
 x = n_natural_numbers(5)
 print(x)
+
+#approch 2
+def n_natural_numbers(n):
+    return n * (n+1)//2
+x = n_natural_numbers(5)
+print(x)
         
